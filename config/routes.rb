66c92055f0087bdd1fae7865b190a8974ac304d0
Justo84 <justin.d.egan@gmail.com>
2015-10-21 12:08:45 -0400

    added meows

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#index'
 
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create] do
+    resources :meows, only: [:create]
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
