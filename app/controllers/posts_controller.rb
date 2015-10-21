0eb37727637352a9ade378b197c405ebc2b5d54f
Justo84 <justin.d.egan@gmail.com>
2015-10-21 11:16:25 -0400

    updated tests and controllers

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,16 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def create
+    # Building a new Post off of the current_user will use some Rails magic to set up the association between the new Post and the User for us.
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to_root_path, notice: "Post creatd successfully."
+    else
+      flash.now[:notice] = "There were some errors with your Post."
+      render :new
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151021004023) do
+ActiveRecord::Schema.define(version: 20151021150650) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "image",       null: false
+    t.integer  "user_id",     null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
