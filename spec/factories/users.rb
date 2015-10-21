c8234f5cab0fa0b1a2d886bff21a4b093d76575b
Justo84 <justin.d.egan@gmail.com>
2015-10-21 10:49:43 -0400

    factory girl for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
-  end
+    # we use a sequence to make sure every User that is created has a unique email.
+    sequence(:email) {|n| "email#{n}@example.com" }
+    password "password123"
 
+  end
 end
